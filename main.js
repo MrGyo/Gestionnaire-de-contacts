b868fb7bd195b11126235e4d18c86ff13dd0f4c1
MrGyo <61049830+MrGyo@users.noreply.github.com>
2020-04-30 19:59:55 +0200

    Update html et css

--- main.js
+++ main.js
@@ -2,21 +2,46 @@
 // comment générer l'intéraction entre un bouton html et le fichier js
 // comment afficher une donnée js dans mon chteumeul
 
+
+class ContactManager{
+    constructor(){
+        this.contacts = [];
+    }
+    add(contact){
+        this.contacts.push(contact);
+    }
+    //Bonus : Si l'email existe déjà, on a un message qui nous informe que l'email est déjà associé à un autre contact.
+    delete(email){
+    }
+    displayContacts(){
+
+    }
+}
+
 class Contact {
     constructor(name, firstName = "", email = "") {
         this.name = name;
         this.firstName = firstName;
+        //Bonus : Si l'email existe déjà, on a un message qui nous informe que l'email est déjà associé à un autre contact.
         this.email = email;
     }
     displayInfo() {
         return ` Name : ${this.name} First Name : ${this.firstName} Email : ${this.email}`;
     }
+
+    modify(){
+
+    }
+
 }
 
 let myFirstContact = new Contact("Parker")
 let mySecondContact = new Contact("Lewis")
 
-console.log(myFirstContact.displayInfo())
+let contacts = new ContactManager();
+contacts.add(myFirstContact);
+contacts.add(mySecondContact);
 
+console.log(myFirstContact.displayInfo())
 
 
