dbba793b59b5bb591a24533c621da7a045358c38
MrGyo <61049830+MrGyo@users.noreply.github.com>
2020-05-05 01:29:19 +0200

    Corrections mineurs des coments et de l'affichage prompt

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,7 @@
 // Création de la classe contact
 class Contact {
     constructor(name, firstName = "", email = "") {
-        //init constructor
+        //initialisation du constructor pour le nom, le prénom et l'email
         this.name = this.checkField(name, "name");
         this.firstName = this.checkField(firstName, "firstName");
         this.email = this.checkField(email, "email");
@@ -11,13 +11,13 @@ class Contact {
         return ` Name : ${this.name} First Name : ${this.firstName} Email : ${this.email}`;
     }
 
-    // Dans cette méthode value ET type sont toujours disponibles (image de la fonction (a, b) => a + b = 1 + 5)
+    // Dans cette méthode, value ET type sont toujours disponibles (image de la fonction (a, b) => a + b = 1 + 5)
     checkField(value, type) {
         switch(type) {
             case "email":
                 if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value) === false)
                 {
-                    let input = prompt("L'email est incorrect, veu " + value + " , veuillez enter un email valide");
+                    let input = prompt("L'email est incorrect " + value + " , veuillez enter un email valide");
                     return this.checkField(input, type);
                 }
                 else
