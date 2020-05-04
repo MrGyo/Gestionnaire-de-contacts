9f6489adc2e6731ba221fb1a5d3c727f1cc93c1d
MrGyo <61049830+MrGyo@users.noreply.github.com>
2020-05-05 00:59:49 +0200

    Update Regex

--- js/contact.js
+++ js/contact.js
@@ -5,22 +5,33 @@ class Contact {
         this.name = this.checkField(name, "name");
         this.firstName = this.checkField(firstName, "firstName");
         this.email = this.checkField(email, "email");
-        // Bonus : Si l'email existe déjà, on a un message qui nous informe que l'email est déjà associé à un autre contact.
     }
+
     displayInfo() {
         return ` Name : ${this.name} First Name : ${this.firstName} Email : ${this.email}`;
     }
+
+    // Dans cette méthode value ET type sont toujours disponibles (image de la fonction (a, b) => a + b = 1 + 5)
     checkField(value, type) {
         switch(type) {
             case "email":
- 
+                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value) === false)
+                {
+                    let input = prompt("L'email est incorrect, veu " + value + " , veuillez enter un email valide");
+                    return this.checkField(input, type);
+                }
+                else
+                {
+                    return value;
+                }
+           
             default:
                 if (value.length <= 2)
                 {
                     let typeName = (type =="name") ? "nom" : "prénom";
                     let promptQuestion = (value == "") ? "Le champ est vide, merci de le remplir" : "Le " + typeName + " " + value + " est invalide (trop petit). Merci d'entrer un autre nom";
                     let input = prompt(promptQuestion);
-                    return this.checkField(input);
+                    return this.checkField(input, type);
                 }
                 else
                 {
